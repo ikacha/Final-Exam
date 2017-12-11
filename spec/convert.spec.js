52ba0d6992c97614f15eaf373dc7ad417db7928d
ikacha <ika.charef@gmail.com>
2017-12-11 15:59:49 +0100

    modif du fichier spec.convert

--- spec/convert.spec.js
+++ spec/convert.spec.js
@@ -31,4 +31,18 @@ describe('Test resultats de la fonction', function() {
         expect(convertisseur('KRW', 1086, 'NZD')).toBe(1.45);
         expect(convertisseur('KRW', 1086, 'USD')).toBe(1);
     });
+    
+    it('doit convertir des NZD dans les autres monnaies', function() {
+        expect(convertisseur('NZD', 1.45, 'EUR')).toBe(0.84);
+        expect(convertisseur('NZD', 1.45, 'USD')).toBe(1);
+        expect(convertisseur('NZD', 1.45, 'KRW')).toBe(1086);
+    });
+
+   it('ne doit pas convertir des mauvais montants', function() {
+        expect(convertisseur('USD', -1, 'EUR')).toBe(null);
+        expect(convertisseur('USD', '', 'EUR')).toBe(null);
+        
+   });
+
+});
 
